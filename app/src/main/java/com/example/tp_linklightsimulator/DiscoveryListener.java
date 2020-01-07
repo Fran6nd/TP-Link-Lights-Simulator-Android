5459ee8358223d2f278ac9a69d303993b03a4420
Fran6nd <francoisnd1@gmail.com>
2020-01-07 21:20:57 +0100

    wip

--- app/src/main/java/com/example/tp_linklightsimulator/DiscoveryListener.java
+++ app/src/main/java/com/example/tp_linklightsimulator/DiscoveryListener.java
@@ -1,39 +1,66 @@
 package com.example.tp_linklightsimulator;
 
+import android.content.Context;
+import android.content.res.Resources;
 import android.util.Log;
 import android.widget.Toast;
 
 import java.io.Console;
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
 import java.net.SocketException;
 import java.net.UnknownHostException;
 
-public class DiscoveryListener implements Runnable {
+import static androidx.core.graphics.drawable.IconCompat.getResources;
+
+public class DiscoveryListener {
     public boolean running = true;
-    public DatagramSocket socket = null;
-    public DiscoveryListener() throws UnknownHostException, SocketException {
-        this.socket = new DatagramSocket(9999, InetAddress.getByName("0.0.0.0"));
-        this.socket.setBroadcast(true);
-    }
-    public String decrypt(byte[] input) {
-        byte firstKey = (byte) 0xAB;
-        byte key = firstKey;
-        byte nextKey;
-        String output = "";
-        for(int i = 0; i < input.length; i++)
-        {
-            nextKey = input[i];
-            input[i] = (byte) (input[i] ^ key);
-            key = nextKey;
+    public static DatagramSocket socket = null;
+    public static  Thread listener;
+    public static  String json_status = R.raw.lb100_status
+    public static boolean init(Context ctx) {
+        try {
+            DiscoveryListener.socket = new DatagramSocket(9999, InetAddress.getByName("0.0.0.0"));
+            DiscoveryListener.socket.setBroadcast(true);
+            Resources res = ctx.getResources();
+            InputStream in_s = res.openRawResource(R.raw.lb100_status);
+
+            byte[] b = new byte[in_s.available()];
+            in_s.read(b);
+            json_status = new String((b));
+            return true;
+        } catch (SocketException e) {
+            e.printStackTrace();
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-        return new String(input);
+        return false;
+
     }
-    @Override
     public void run()
     {
+        listener  = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                    loop();
+
+            }
+        });
+    }
+    public static void stop(){
+        try {
+            listener.interrupt();
+            listener.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+    public static void loop(){
         System.out.println("Now listening");
         while(!Thread.currentThread().isInterrupted())
         {
@@ -41,16 +68,12 @@ public class DiscoveryListener implements Runnable {
             DatagramPacket packet = new DatagramPacket(buf, buf.length);
             try {
                 socket.receive(packet);
-                System.out.println(this.decrypt(packet.getData()));
+                System.out.println(tplink.decrypt(packet.getData()));
+                socket.send(new DatagramPacket(tplink.encrypt(json_status),json_status.getBytes().length, packet.getAddress(), packet.getPort() ));
 
             } catch (IOException e) {
                 e.printStackTrace();
             }
         }
-
-    }
-    public void shutdown()
-    {
-        this.running = false;
     }
 }
