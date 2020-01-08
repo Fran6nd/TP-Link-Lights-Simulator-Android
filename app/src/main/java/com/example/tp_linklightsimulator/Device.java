a1fa7f2cbc50a40d44a72d924d3ef248c3a75320
Fran6nd <francoisnd1@gmail.com>
2020-01-08 15:11:26 +0100

    Improved API

--- app/src/main/java/com/example/tp_linklightsimulator/Device.java
+++ app/src/main/java/com/example/tp_linklightsimulator/Device.java
@@ -37,6 +37,7 @@ public class Device {
     public static ImageView view;
     public static AppCompatActivity ctx;
     public boolean running = true;
+    public static int brightness = 255;
 
     public static boolean init(AppCompatActivity ctx, String name, ImageView lamp) {
         Device.name = name;
@@ -105,7 +106,14 @@ public class Device {
     }
 
     public static void sendInfos(DatagramPacket packet) throws IOException {
-        byte[] msg = tplink.encrypt(String.format(json_status, Device.deviceId, Device.name, Device.state));
+        byte[] msg = tplink.encrypt(String.format(json_status, Device.name, Device.deviceId, Device.state));
+
+        socket.send(new DatagramPacket(msg, msg.length, packet.getAddress(), packet.getPort()));
+
+    }
+
+    public static void sendString(DatagramPacket packet, String s) throws IOException {
+        byte[] msg = tplink.encrypt(s);
 
         socket.send(new DatagramPacket(msg, msg.length, packet.getAddress(), packet.getPort()));
 
@@ -120,9 +128,11 @@ public class Device {
         // Decoding the image two resource into a Bitmap
         Bitmap imageTwo = BitmapFactory.decodeResource(ctx.getResources(), R.drawable.light_contour);
         if (state == 1) {
-            imageTwo = replaceColor(imageTwo, Color.WHITE, ORANGE);
+            imageTwo = setColor(imageTwo, (ORANGE & 0x00FFFFFF ) | (((byte)brightness)<<8*3));
+            System.out.println(String.format("0x%08X", (((byte)0xFA<<8*3))));
+
         } else {
-            imageTwo = replaceColor(imageTwo, ORANGE, Color.WHITE);
+            imageTwo = setColor(imageTwo, Color.WHITE);
         }
 
         // Here we construct the canvas with the specified bitmap to draw onto
@@ -135,6 +145,9 @@ public class Device {
             }
         });
 
+    }
+    public static void setBrightness(int b){
+        Device.brightness = b;
     }
 
     public static Bitmap replaceColor(Bitmap src, int fromColor, int targetColor) {
@@ -158,6 +171,27 @@ public class Device {
 
         return result;
     }
+    public static Bitmap setColor(Bitmap src, int targetColor) {
+        if (src == null) {
+            return null;
+        }
+        // Source image size
+        int width = src.getWidth();
+        int height = src.getHeight();
+        int[] pixels = new int[width * height];
+        //get pixels
+        src.getPixels(pixels, 0, width, 0, 0, width, height);
+
+        for (int x = 0; x < pixels.length; ++x) {
+            pixels[x] = ((pixels[x] & 0xFF000000) != 0) ? targetColor : pixels[x];
+        }
+        // create result bitmap output
+        Bitmap result = Bitmap.createBitmap(width, height, src.getConfig());
+        //set pixels
+        result.setPixels(pixels, 0, width, 0, 0, width, height);
+
+        return result;
+    }
 
 
     public static void loop() {
@@ -166,19 +200,36 @@ public class Device {
             byte[] buf = new byte[512];
             Arrays.fill(buf, (byte) 0);
             DatagramPacket packet = new DatagramPacket(buf, buf.length);
+            System.out.println(tplink.decrypt(packet.getData()));
             try {
                 socket.receive(packet);
                 String json = tplink.decrypt(packet.getData());
                 JSONObject obj = new JSONObject(json);
-                JSONObject instruct = obj.getJSONObject("system");
-                if (instruct.has("get_sysinfo")) {
-                    System.out.println("Get infos");
-                } else if (instruct.has("set_relay_state")) {
-                    set_state(instruct.getJSONObject("set_relay_state").getInt("state"));
+                if(obj.has("system")) {
+                    JSONObject instruct = obj.getJSONObject("system");
+                    if (instruct.has("get_sysinfo")) {
+                        Device.sendInfos(packet);
+                    } else if (instruct.has("set_relay_state")) {
+                        set_state(instruct.getJSONObject("set_relay_state").getInt("state"));
+                        Device.sendInfos(packet);
+                    }
                 }
-                String instruction;
-                System.out.println(json);
-                Device.sendInfos(packet);
+                else if(obj.has("smartlife.iot.smartbulb.lightingservice")){
+                    if(obj.getJSONObject("smartlife.iot.smartbulb.lightingservice").has("transition_light_state")) {
+                        JSONObject instruct = obj.getJSONObject("smartlife.iot.smartbulb.lightingservice").getJSONObject("transition_light_state");
+                        if(instruct.has("brightness"))
+                        {
+                            setBrightness(instruct.getInt("brightness"));
+                        }
+
+                        set_state(instruct.getInt("on_off"));
+                        String str =" {\"smartlife.iot.smartbulb.lightingservice\":{\"transition_light_state\":{\"on_off\":1,\"mode\":\"normal\",\"hue\":120,\"saturation\":65,\"color_temp\":0,\"brightness\":10,\"err_code\":0}}} " ;
+                        sendString(packet, str);
+
+                    }
+
+                }
+
 
 
             } catch (SocketTimeoutException exception) {
